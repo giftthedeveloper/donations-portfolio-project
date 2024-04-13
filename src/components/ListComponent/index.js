07bb46d83ff84fd6459fa1ff233570e7b044e64a
giftthedeveloper <giftjeremiah001@gmail.com>
2024-04-13 13:05:36 +0100

    feat: completed design for list component

--- src/components/ListComponent/index.js
+++ src/components/ListComponent/index.js
@@ -1,21 +1,39 @@
 import React from 'react'
-import { MainContainer } from './ListComponentElements'
+import { MainContainer, ItemContainer, VerticalLine, AccessibilityIcon, NatureIcon, LocationIcon, HouseIcon, MainText, BodyText} from './ListComponentElements'
 
-// const ListItem = ({ iconSrc, name, text }) => (
-//   <ItemContainer>
-//     <Icon src={iconSrc} alt={name} />
-//     <div>
-//       <Name>{name}</Name>
-//       <Text>{text}</Text>
-//     </div>
-//   </ItemContainer>
-// );
+
 
 function ListComponent() {
   return (
     <MainContainer>
+      <ItemContainer>
+          <AccessibilityIcon />
+          <MainText>Lorem ipsum</MainText>
+          <BodyText href="#">Read More</BodyText>
+
+      </ItemContainer>
+      <VerticalLine />
+
+      <ItemContainer>
+          <NatureIcon />
+          <MainText>Lorem ipsum</MainText>
+          <BodyText>Read More</BodyText>
+      </ItemContainer>
+      <VerticalLine />
+      <ItemContainer>
       
-        
+          < LocationIcon/>
+          <MainText>Lorem ipsum</MainText>
+          <BodyText>Learn More</BodyText>
+          
+      </ItemContainer>
+      <VerticalLine />
+      
+      <ItemContainer>
+          <HouseIcon />
+          <MainText>Lorem ipsum</MainText>
+          <BodyText>Learn more</BodyText>
+      </ItemContainer>
     </MainContainer>
   )
 }
