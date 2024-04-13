ba90149883fe3d3748abf7d5c5f37d36a2d72d55
giftthedeveloper <giftjeremiah001@gmail.com>
2024-04-13 15:05:56 +0100

    feat: worled on blog section

--- src/components/BlogSection/index.js
+++ src/components/BlogSection/index.js
@@ -1,12 +1,31 @@
 import React from 'react'
-import { BlogContainer, MainComponent } from './BlogElements'
+import { ItemContainer, MainItemComponent,ButtonText, Button, MainText,BlogImgThree, BlogImgTwo, BodyText, BlogImgOne, MainComponent, ButtonContainer } from './BlogElements'
 
 function Blog() {
   return (
     <MainComponent >
-        <BlogContainer>
-            
-        </BlogContainer>
+      <MainItemComponent>
+      <ItemContainer>
+          <BlogImgOne />
+          <MainText>Blog Title 1</MainText>
+          <BodyText href="#">Read More</BodyText>
+
+      </ItemContainer>
+
+      <ItemContainer>
+          <BlogImgTwo />
+          <MainText>Blog Title 2</MainText>
+          <BodyText href="#">Read More</BodyText>
+
+      </ItemContainer>
+      <ItemContainer>
+          <BlogImgThree />
+          <MainText>Blog Title 3</MainText>
+          <BodyText href="#">Read More</BodyText>
+
+      </ItemContainer>
+      </MainItemComponent>
+      <ButtonContainer><Button><ButtonText>Read the blog</ButtonText></Button></ButtonContainer>
     </MainComponent>
   )
 }
